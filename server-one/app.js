b3789a674a6a75b044e43a5f2dc0abdb865f3026
yliysi <catrina.koskinen@gmail.com>
2022-10-28 13:06:03 +0300

    Adding server-one folder assigment

---
+++ server-one/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/catinfo', (req, res) => {
+    const cat = {
+        name: 'Frank',
+        birthdate: '2010-12-25',
+        weight: 5,
+    };
+    res.json(cat);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
