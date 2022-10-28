3fabc4e2da8ade17ead307c855546d2a1d570fea
yliysi <catrina.koskinen@gmail.com>
2022-10-28 14:34:19 +0300

    Adding server-two folder

---
+++ server-two/app.js
@@ -0,0 +1,26 @@
+'use strict';
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine','pug');
+app.set('views','./views');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index')
+});
+
+app.get('/catinfo', (req, res) => {
+    const cat = {
+        name: 'Frank',
+        birthdate: '2010-12-25',
+        weight: 5,
+    };
+    res.json(cat);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
